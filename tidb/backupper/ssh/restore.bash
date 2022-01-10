df3ffab499943d51851e7adfa7391558cddef832
Liu Cong <innerr@gmail.com>
2022-01-10 23:46:46 +0800

    make backupper a choice
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- tidb/backupper/ssh/restore.bash
+++ tidb/backupper/ssh/restore.bash
@@ -1,7 +1,8 @@
 set -euo pipefail
-. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../helper/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../../helper/helper.bash"
 
 env=`cat "${1}/env"`
+shift
 
 # export: $pri_key, $user, $cnt, $hosts, $deploy_dirs, $data_dirs
 get_instance_info "${env}" 'true'
