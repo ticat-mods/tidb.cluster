4c2d8ca5633fb54a18533c0bc3afe5aa3cf8b238
Liu Cong <innerr@gmail.com>
2022-01-23 19:40:39 +0800

    remove logging in command deploy.set.*
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- br/ssh/list.bash
+++ br/ssh/list.bash
@@ -1,5 +1,5 @@
 set -euo pipefail
-. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../helper/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../helper/helper.bash"
 
 env=`cat "${1}/env"`
 
