719baf52cdf621975a4b330adc9bf9872d0c0e79
Liu Cong <innerr@gmail.com>
2022-02-17 01:24:00 +0800

    support depose flag when generating tiup-yaml from env kvs
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- deploy/gen/tiup-yaml.py
+++ deploy/gen/tiup-yaml.py
@@ -5,11 +5,14 @@ sys.path.append('../../helper/python.helper')
 sys.path.append('../../helper/tiup.helper')
 
 from ticat import Env
+from strs import to_true
 from topology import TiUPYaml
 
 def main():
 	env = Env()
-	yaml = TiUPYaml(env)
+	depose_kvs = to_true(env.must_get('deploy.kvs.depose'))
+
+	yaml = TiUPYaml(env, depose_kvs = depose_kvs)
 	text, path = yaml.save()
 
 	env.set('tidb.tiup.yaml', path)
@@ -17,6 +20,5 @@ def main():
 
 	print(text + '\n')
 	print('tidb.tiup.yaml=' + path)
-
 
 main()
